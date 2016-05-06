229609d57859efb4c60c598bebab71584d97adb3
Dandresfs <dandresfsoto@gmail.com>
2016-05-06 14:48:17 -0500

    oferta admin title

--- ofertas/admin.py
+++ ofertas/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from ofertas.models import Oferta, Categoria
 
 # Register your models here.
-admin.site.register(Oferta)
+class OfertaAdmin(admin.ModelAdmin):
+    list_display = ['empresa','titulo','descripcion']
+admin.site.register(Oferta,OfertaAdmin)
 admin.site.register(Categoria)
