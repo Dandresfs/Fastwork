1feed4497e3265b2e429b25a0635452cac006faf
Dandresfs <dandresfsoto@gmail.com>
2016-05-13 18:08:54 -0500

    Detail age

--- rest/views.py
+++ rest/views.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from hv.models import Experiencia, Formacion
 from rest.serializers import ExperienciaSerializer, FormacionSerializer, OfertaSerializer
 from rest_framework import mixins, pagination, response
@@ -13,6 +14,7 @@ import operator
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
+from datetime import datetime
 
 
 def departamentos(request):
@@ -173,10 +175,88 @@ class ComparativaApiView(APIView):
         oferta = self.get_object(pk)
 
         aplicados = oferta.aplicacion.all()
-        municipios = list(aplicados.values_list('ciudad',flat=True))
+        aplicado_request = oferta.aplicacion.get(id=request.user.id)
 
-        x = oferta.municipio
-        municipios_si = municipios.count(x)
+        municipios = list(aplicados.values_list('ciudad',flat=True))
+        municipios_si = municipios.count(oferta.municipio)
         municipios_no = len(municipios)-municipios_si
 
-        return Response({'residencia':{'total':aplicados.count(),'si':municipios_si,'no':municipios_no}})
+        r1 = 0
+        r2 = 0
+        r3 = 0
+        r4 = 0
+        r5 = 0
+        r6 = 0
+        r7 = 0
+
+        for aplicado in aplicados:
+            nacimiento = aplicado.fecha_nacimiento
+            if nacimiento != None:
+                edad = self.calcular_edad(nacimiento)
+                if edad >= 18 and edad < 25:
+                    r1 += 1
+                if edad >= 25 and edad < 30:
+                    r2 += 1
+                if edad >= 30 and edad < 35:
+                    r3 += 1
+                if edad >= 35 and edad < 40:
+                    r4 += 1
+                if edad >= 40 and edad < 45:
+                    r5 += 1
+                if edad >= 45 and edad < 50:
+                    r6 += 1
+                if edad >= 50:
+                    r7 += 1
+
+        if aplicado_request.fecha_nacimiento != None:
+            edad = self.calcular_edad(aplicado_request.fecha_nacimiento)
+
+            if edad >= 18 and edad < 25:
+                m = "18 y 24"
+                r = r1
+            if edad >= 25 and edad < 30:
+                m = "25 y 29"
+                r = r2
+            if edad >= 30 and edad < 35:
+                m = "30 y 34"
+                r = r3
+            if edad >= 35 and edad < 40:
+                m = "35 y 39"
+                r = r4
+            if edad >= 40 and edad < 45:
+                m = "40 y 44"
+                r = r5
+            if edad >= 45 and edad < 50:
+                m = "45 y 49"
+                r = r6
+            if edad >= 50:
+                m = "50 o mas"
+                r = r7
+
+            edad_porcentaje = self.percent(aplicados.exclude(fecha_nacimiento = None).count(),r)
+            rango_edad = "El "+str(edad_porcentaje)+"% de los aspirantes tiene entre " + m + " años."
+        else:
+            rango_edad = "Debes actualizar esta informacion en tu perfil"
+
+
+
+
+
+
+
+        return Response({'residencia':{'total':aplicados.count(),
+                                       'si' : municipios_si,
+                                       'no' : municipios_no,
+                                       'si_porcentaje' : self.percent(aplicados.count(),municipios_si),
+                                       'no_porcentaje' : self.percent(aplicados.count(),municipios_no),
+                                       'ciudad' : oferta.municipio},
+                         'edad':{'total':aplicados.count(),
+                                 'edad':[r1,r2,r3,r4,r5,r6,r7],
+                                 'rango':rango_edad}
+                        })
+
+    def percent(self,total,value):
+        return round((value*100)/total)
+
+    def calcular_edad(self,date):
+        return int((datetime.date(datetime.now()) - date).days/365.2425)
