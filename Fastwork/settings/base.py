77dff4a95cb81d3411b6881ca404995b5a3aa064
Dandresfsoto <dandresfsoto@gmail.com>
2016-06-24 18:14:11 -0500

    Celery email task

--- Fastwork/settings/base.py
+++ Fastwork/settings/base.py
@@ -6,8 +6,8 @@ import os
 from celery.schedules import crontab
 
 # CELERY STUFF
-BROKER_URL = 'amqp://guest:guest@localhost:5672//'
-CELERY_RESULT_BACKEND = 'amqp://guest:guest@localhost:5672//'
+BROKER_URL = 'amqp://'
+CELERY_RESULT_BACKEND = 'rpc://'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
