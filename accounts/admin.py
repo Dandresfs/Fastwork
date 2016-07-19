af4224497d7c508ecabf469930d63b694ef4d35d
Dandresfsoto <dandresfsoto@gmail.com>
2016-07-19 08:37:36 -0500

    Selector datatables cpe ok 5

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from .models import User, PreUser
 
 # Register your models here.
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['email']
+
+
+admin.site.register(User, UserAdmin)
 
 class PreUserAdmin(admin.ModelAdmin):
     list_display = ['email','password','mail_send']
+    search_fields = ['email']
 
 admin.site.register(PreUser,PreUserAdmin)
