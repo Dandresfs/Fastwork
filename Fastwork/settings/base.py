9d17061c6d009e5d0933a73b4c43e902d6844626
Dandresfs <dandresfsoto@gmail.com>
2017-01-17 15:08:55 -0500

    Pago 38

--- Fastwork/settings/base.py
+++ Fastwork/settings/base.py
@@ -6,8 +6,8 @@ import os
 from celery.schedules import crontab
 
 # CELERY STUFF
-BROKER_URL = 'amqp://'
-CELERY_RESULT_BACKEND = 'rpc://'
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
