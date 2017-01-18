2e5a8316a54e1dc352d795eb1e329cb650fa1b16
Dandresfs <dandresfsoto@gmail.com>
2017-01-18 10:12:17 -0500

    Email ok 2

--- Fastwork/settings/base.py
+++ Fastwork/settings/base.py
@@ -45,10 +45,10 @@ ADMINS = [('DIEGO','sistemas@asoandes.org')]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'noresponder.fastworkcolombia@gmail.com'
-EMAIL_HOST_PASSWORD = 'maquinas2008'
-EMAIL_PORT = 587
+EMAIL_HOST = 'r113.websiteservername.com'
+EMAIL_HOST_USER = 'no-responder@fastworkcolombia.com'
+EMAIL_HOST_PASSWORD = 'fastworkcolombia'
+EMAIL_PORT = 25
 DEFAULT_FROM_EMAIL = 'Fast Work Colombia<no-responder@fastworkcolombia.com>'
 
 
